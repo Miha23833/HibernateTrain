6b0258a8e5c002a7393af1b837c79e00f411190b
Byoliee <mu.korepanov@gmail.com>
2020-10-14 23:53:26 +0300

    changed logger

--- src/test/java/com/exactpro/test/common/CommonUnitTests.java
+++ src/test/java/com/exactpro/test/common/CommonUnitTests.java
@@ -1,13 +1,14 @@
 package com.exactpro.test.common;
 
 import com.exactpro.connection.DBConnection;
+import com.exactpro.loggers.StaticLogger;
 import org.apache.log4j.Logger;
 
 import java.sql.SQLException;
 
 public class CommonUnitTests {
 
-    private static final Logger logger = Logger.getLogger(DBConnection.class);
+    private static final Logger infoLogger = StaticLogger.infoLogger;
 
     public static void cleanDB(String url, String user, String password) throws SQLException, ClassNotFoundException {
         DBConnection.setConnectionData(url, user, password);
@@ -19,6 +20,6 @@ public class CommonUnitTests {
         DBConnection.executeNonResult("DELETE FROM DEALS");
         DBConnection.executeNonResult("ALTER TABLE DEALS AUTO_INCREMENT = 0");
 
-        logger.info("Database " + url + "was cleaned by user " + user);
+        infoLogger.info("Database " + url + "was cleaned by user " + user);
     }
 }
