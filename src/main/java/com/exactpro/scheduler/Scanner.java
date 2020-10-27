88f47100d4d6c28456afc449c104a036e382062b
Byoliee <mu.korepanov@gmail.com>
2020-10-27 22:13:48 +0300

    scanAndLoadData() realization

--- src/main/java/com/exactpro/scheduler/Scanner.java
+++ src/main/java/com/exactpro/scheduler/Scanner.java
@@ -1,13 +1,17 @@
 package com.exactpro.scheduler;
 
+import com.exactpro.DAO.SingleSessionFactory;
 import com.exactpro.loggers.StaticLogger;
-import com.opencsv.CSVReader;
 import org.apache.log4j.Logger;
+import org.hibernate.Session;
 
-import java.io.*;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
+import java.nio.file.StandardCopyOption;
+import java.sql.SQLException;
 
 public class Scanner {
     private final Logger infoLogger = StaticLogger.infoLogger;
@@ -72,6 +76,8 @@ public class Scanner {
         this.freshData = freshData;
         this.insertedData = insertedData;
         this.rejectedData = rejectedData;
+
+        createFolders();
     }
 
     public Scanner(String delimiter, String sourceRoot) throws IOException {
@@ -79,16 +85,47 @@ public class Scanner {
 
         checkRelatively(sourceRoot);
         this.sourceRoot = sourceRoot;
+
+        createFolders();
     }
 
-    private List<String[]> loadCSV() throws FileNotFoundException {
+    public void scanAndLoadData() throws ClassNotFoundException, IOException {
 
         FilenameFilter filter = (dir, name) -> name.endsWith(".csv");
         File currentPath = new File(sourceRoot + freshData);
         String [] filenames = currentPath.list(filter);
 
+        AbstractDataLoader loader = new DealDataLoader();
 
+        Session session = SingleSessionFactory.getInstance().openSession();
+        for (String filename : filenames) {
+            try {
+                loader.insertData(session, sourceRoot + freshData, filename);
 
+                Files.move(Paths.get(sourceRoot + freshData + filename + ".csv"),
+                        Paths.get(sourceRoot + insertedData),
+                        StandardCopyOption.REPLACE_EXISTING);
+
+            }catch (SQLException e){
+                warnLogger.error(e);
+
+                Files.move(Paths.get(sourceRoot + freshData + filename + ".csv"),
+                        Paths.get(sourceRoot + rejectedData),
+                        StandardCopyOption.REPLACE_EXISTING);
+
+                if (session.getTransaction().isActive()) {
+                    session.getTransaction().rollback();
+                }
+
+            } catch (ClassNotFoundException e) {
+                warnLogger.error(e);
+                throw new ClassNotFoundException(e.toString());
+            }
+        }
+        if (session.getTransaction().isActive()) {
+            session.getTransaction().commit();
+        }
+        session.close();
     }
 
 }
