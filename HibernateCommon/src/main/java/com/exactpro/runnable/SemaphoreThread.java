111ea475ab69f66ec0a8eb2a8558a33b85f1a5ee
Byoliee <mu.korepanov@gmail.com>
2020-10-30 09:05:21 +0300

    add this file

--- HibernateCommon/src/main/java/com/exactpro/runnable/SemaphoreThread.java
+++ HibernateCommon/src/main/java/com/exactpro/runnable/SemaphoreThread.java
@@ -1,10 +1,36 @@
 package com.exactpro.runnable;
 
-public class CSVLoadThread implements Runnable {
+import com.exactpro.functional.Function;
+import com.exactpro.loggers.StaticLogger;
+import org.apache.log4j.Logger;
 
-    public CSVLoadThread()
+import java.io.IOException;
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreThread<T> extends Thread {
+
+    final Function<T> function;
+    final T value;
+    final Semaphore semaphore;
+
+    Logger warnLogger = StaticLogger.warnLogger;
+
+    public SemaphoreThread(Function<T> function, T value, Semaphore semaphore){
+        this.function = function;
+        this.value = value;
+        this.semaphore = semaphore;
+    }
     @Override
     public void run() {
-
+        try {
+            semaphore.acquire();
+            function.execute(value);
+        } catch (InterruptedException | IOException | ClassNotFoundException e) {
+            warnLogger.error(e);
+            throw new RuntimeException(e);
+        }
+        finally {
+            semaphore.release();
+        }
     }
 }
