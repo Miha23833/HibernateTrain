decc4eb6407b26dc898b52d3d511ab5fded63e9e
Byoliee <mu.korepanov@gmail.com>
2020-10-30 09:05:32 +0300

    add this file

--- Scheduler/src/main/java/com/exactpro/scheduler/Scheduler.java
+++ Scheduler/src/main/java/com/exactpro/scheduler/Scheduler.java
@@ -1,7 +1,67 @@
 package com.exactpro.scheduler;
 
+import com.exactpro.functional.Function;
+import com.exactpro.loggers.StaticLogger;
+import com.exactpro.runnable.SemaphoreThread;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.Semaphore;
+
 public class Scheduler {
 
+    final Scanner scanner;
 
+    static final Logger warnLogger = StaticLogger.warnLogger;
+    static final Logger infoLogger = StaticLogger.infoLogger;
+
+    Set<String> foldersInProcessPool = new HashSet<>();
+
+    final Semaphore semaphore;
+
+    public Scheduler() throws IOException {
+
+        scanner = new Scanner(Config.getDelimiter(),
+                Config.getSourceRoot(),
+                Config.getFreshData(),
+                Config.getInsertedData(),
+                Config.getRejectedData());
+
+        if (Config.getMaxThreadPool() < 1) {
+            IllegalArgumentException exception = new IllegalArgumentException("MaxThreadPool must me at least 1.");
+            warnLogger.error(exception);
+            throw exception;
+        }
+
+        semaphore = new Semaphore(Config.getMaxThreadPool());
+
+    }
+
+    private void run() {
+        for (String fileName: scanner.getCSVFilenames()) {
+            if (!foldersInProcessPool.contains(fileName)) {
+                Function<String> lambda = scanner::loadDealsFromCSV;
+                Thread CSVLoadThread = new SemaphoreThread<>(lambda, fileName, semaphore);
+                foldersInProcessPool.add(fileName);
+                CSVLoadThread.start();
+                infoLogger.info(String.format("Thread %s started to load CSV to DB", CSVLoadThread.getName()));
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        while (true) {
+            Scheduler scheduler = new Scheduler();
+            scheduler.run();
+            try {
+                Thread.sleep(Config.getMaxThreadPool());
+            } catch (InterruptedException e) {
+                warnLogger.error(e);
+                throw new InterruptedException(e.toString());
+            }
+        }
+    }
 
 }
