87b468c38f3c6b76a8eea179e3986775b5fe911f
Byoliee <mu.korepanov@gmail.com>
2020-11-11 00:09:55 +0300

    refactor FileDataKeeper

--- Scheduler/src/main/java/com/exactpro/scheduler/dataExchanger/FileDataKeeper.java
+++ Scheduler/src/main/java/com/exactpro/scheduler/dataExchanger/FileDataKeeper.java
@@ -1,27 +1,54 @@
 package com.exactpro.scheduler.dataExchanger;
 
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-public class FileDataKeeper {
-    private static final Map<String, Map<String, List<String>>> files = new ConcurrentHashMap<>();
+public class FileDataKeeper<T>{
 
-    public static Map<String, Map<String, List<String>>> getAllData(){
-        return files;
+    ConditionWaiter waiter;
+    private final List<T> data = new LinkedList<>();
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+
+    public FileDataKeeper(int queueSize) {
+        waiter  = new ConditionWaiter(() -> data.size() < queueSize, 300);
     }
 
-    public static void add(String filename, Map<String, List<String>> data){
-        files.put(filename, data);
+    public void put(T entity) {
+        try {
+            waiter.await();
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        lock.writeLock().lock();
+        try {
+            data.add(entity);
+        }
+        finally {
+            lock.writeLock().unlock();
+        }
     }
 
-    public static Set<String> keySet(){
-        return files.keySet();
+    public List<T> getAllData(){
+        lock.writeLock().lock();
+        try {
+            List<T> copyData = new LinkedList<>(data);
+            data.clear();
+            return copyData;
+        }
+        finally {
+            lock.writeLock().unlock();
+        }
     }
 
-    public static Map<String, List<String>> get(String key){
-        return files.get(key);
+    public T getLast(){
+        if (data.size() > 0) {
+            return data.remove(data.size() - 1);
+        }
+        else{
+            return null;
+        }
     }
-
 }
