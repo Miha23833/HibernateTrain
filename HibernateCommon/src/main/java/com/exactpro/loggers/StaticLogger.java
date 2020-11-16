c0e19fd1fe55429a9cae5c634b135b2ba8be5953
Byoliee <mu.korepanov@gmail.com>
2020-11-16 14:46:39 +0300

    configured logs

--- HibernateCommon/src/main/java/com/exactpro/loggers/StaticLogger.java
+++ HibernateCommon/src/main/java/com/exactpro/loggers/StaticLogger.java
@@ -1,6 +1,9 @@
 package com.exactpro.loggers;
 
+import org.apache.log4j.Logger;
+
 public class StaticLogger {
-    public static final org.apache.log4j.Logger infoLogger = org.apache.log4j.Logger.getLogger("infoLogger");
-    public static final org.apache.log4j.Logger warnLogger = org.apache.log4j.Logger.getLogger("warnLogger");
+    public static final Logger infoLogger = Logger.getLogger("infoLogger");
+    public static final Logger warnLogger = Logger.getLogger("warnLogger");
+    public static final Logger csvRejectedDataLogger = Logger.getLogger("rejectedCSVLogger");
 }
