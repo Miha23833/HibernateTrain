8a98bfc4782b85ff40553ff0b76471de0f53822e
Byoliee <mu.korepanov@gmail.com>
2020-11-21 01:09:17 +0300

    add webapp

--- ServletSite/src/main/java/com/exactpro/site/servlets/MainServlet.java
+++ ServletSite/src/main/java/com/exactpro/site/servlets/MainServlet.java
@@ -23,6 +23,12 @@ public class MainServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello, world! MF");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello, world! MF");
     }
 
     @Override
