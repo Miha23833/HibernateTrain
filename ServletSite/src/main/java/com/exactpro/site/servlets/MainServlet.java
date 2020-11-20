698ee9531341240a64b3fd2b90b6d1e6cf8d4533
Byoliee <mu.korepanov@gmail.com>
2020-11-21 00:48:57 +0300

    lets do servlets!

---
+++ ServletSite/src/main/java/com/exactpro/site/servlets/MainServlet.java
@@ -0,0 +1,32 @@
+package com.exactpro.site.servlets;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(urlPatterns = {"/hello-world"})
+public class MainServlet extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
