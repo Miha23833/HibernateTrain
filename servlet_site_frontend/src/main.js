104f838626154f19a3de096fb7c92400409c81b0
Byoliee <mu.korepanov@gmail.com>
2020-12-03 16:19:02 +0300

    add fields

--- servlet_site_frontend/src/main.js
+++ servlet_site_frontend/src/main.js
@@ -1,7 +1,39 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import {ENTITY} from './enums/ENTITIES'
+import {USER_ACTION} from "@/enums/USER_ACTIONS";
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+  state: {
+    currentEntity: ENTITY.Customer,
+    currentUserAction: USER_ACTION.SELECT,
+    errorMessage: '',
+  },
+  mutations: {
+    setCurrentEntity(state, payload){
+      if (payload in ENTITY) {
+        state.currentEntity = payload;
+      }
+    },
+
+    setCurrentUserAction(state, payload){
+      if (payload in USER_ACTION){
+        state.currentUserAction = payload;
+      }
+    },
+
+    setErrorMessage(state, payload){
+      if (payload instanceof String){
+        state.errorMessage = payload;
+      }
+    }
+  }
+
+}  )
 
 new Vue({
   render: h => h(App),
